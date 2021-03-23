7b81e2873b270b1cd8e78fcf9240f3642be98dcc
code-hh <81086753+code-hh@users.noreply.github.com>
2021-03-23 23:43:23 +0900

    입력.

--- BaekJoon/BaekJoon/baekjoon14888.cpp
+++ BaekJoon/BaekJoon/baekjoon14888.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ios_base::sync_with_stdio(false);
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> arr;
+	arr.reserve(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int value;
+		cin >> value;
+		arr.push_back(value);
+	}
+
+	int op[4];
+	// + , -, *, /
+	cin >> op[0] >> op[1] >> op[2] >> op[3];
+
+
+	return 0;
 }
