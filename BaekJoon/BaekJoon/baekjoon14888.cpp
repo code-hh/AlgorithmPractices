1fb59b599de8bb7b71ea5a421e47e76d8108a215
code-hh <81086753+code-hh@users.noreply.github.com>
2021-03-25 23:34:29 +0900

    beakjoon14888 solved
    beakjoon14888 solved
    
    reference : std::next_permutation()

--- BaekJoon/BaekJoon/baekjoon14888.cpp
+++ BaekJoon/BaekJoon/baekjoon14888.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 
 using namespace std;
+
+int n;
+int minValue = INT_MAX;
+int maxValue = INT_MIN;
+
+void dfs(vector<int>& arr, int index, int result, int plus, int minus, int multi, int divide)
+{
+	if (n == index)
+	{
+		if (minValue > result) minValue = result;
+		if (maxValue < result) maxValue = result;
+	}
+	
+	if (plus > 0) dfs(arr, index + 1, result + arr[index], plus - 1, minus, multi, divide);
+	if (minus > 0) dfs(arr, index + 1, result - arr[index], plus, minus - 1, multi, divide);
+	if (multi > 0) dfs(arr, index + 1, result * arr[index], plus, minus, multi - 1, divide);
+	if (divide > 0) dfs(arr, index + 1, result / arr[index], plus, minus, multi, divide - 1);
+}
 
 int main()
 {
@@ -10,7 +30,7 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
+
 	cin >> n;
 
 	vector<int> arr;
@@ -27,6 +47,12 @@ int main()
 	// + , -, *, /
 	cin >> op[0] >> op[1] >> op[2] >> op[3];
 
+	dfs(arr, 1, arr[0], op[0], op[1], op[2], op[3]);
+
+	cout << maxValue << endl;
+	cout << minValue << endl;
+
+	// next_permutation(arr.begin(), arr.end()) 함수를 사용해서 더 간단하게 만들수 있음
 
 	return 0;
 }
